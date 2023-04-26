cdc7345a851a208f762f69cf9880c8660e04e127
Leonardo <leolnn14@gmail.com>
2023-04-26 11:00:26 -0300

    Commit aula 2

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,44 @@
-<?php
+<html>
+    <head>
+        <title> PHP test </title>
+    </head>
+    <body>
+        <?php
+            /*$contador = 1;
+            $mensagem = "Olá";
+            $versao = array('1.1','2','3.4');
+            print_r($versao)
+            $primeiro_nome = "Maria";
+            echo $primeiro_nome;
+            echo '<br   />';
+            $ultimo_nome = "Lourdes";
+            echo $ultimo_nome;
+            $time = array('Maria','José','Ana');
 
-//phpinfo();
-echo 'oi';
+            echo $time[0];
+            echo '<br ?>';
+
+            print_r($time);*/
+
+            /*echo 'a: [' . TRUE . ']<br />';
+            echo 'a: [' . FALSE . ']<br />';
+            echo '<br   />';
+            echo 'a: [' . (20 > 9) . ']<br />';
+            echo 'b: [' . (5 == 6) . ']<br />';
+            echo 'c: [' . (1 == 0) . ']<br />';
+            echo 'd: [' . (1 === 1) . ']<br />';
+            echo '<br   />';
+            $idade = 30;
+            echo 'e: [' . $idade . ']<br />';*/
+
+            /*$saldo = 10;
+            if ($saldo<100){
+                $deposito = 1000;
+                $saldo +=$deposito;
+            }
+
+            echo $saldo;*/
+            
+        ?>
+    </body>
+</html>
